03a4be144d60e542780c869b8b185bfa5b6d1af1
franklinAnozie <cfanozie@gmail.com>
2024-09-02 23:50:32 +0100

    2.first api

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ import { AppController } from '../controllers';
 const router = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
-  app.post('/users', UserController.postNew);
 };
 
 module.exports = router;
