00005bcc325e2ec6d42e5c0d60d38495ed7e7340
franklinAnozie <cfanozie@gmail.com>
2024-09-02 17:31:06 +0100

    attempted task 1

---
+++ utils/redis.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor () {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(error.message);
+    });
+  }
+
+  isAlive () {
+    if (this.client.ready) return true;
+    return false;
+  }
+
+  async get (key) {
+    try {
+      const value = await new Promise((resolve, reject) => {
+        this.client.get(key, (err, val) => {
+          if (err) reject(err);
+          else resolve(val);
+        });
+      });
+      return value;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async set (key, value, dur) {
+    try {
+      await new Promise((resolve, reject) => {
+        this.client.set(key, value, 'EX', dur, (err, val) => {
+          if (err) reject(err);
+          else resolve(val);
+        });
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async del (key) {
+    try {
+      await new Promise((resolve, reject) => {
+        this.client.del(key, (err, val) => {
+          if (err) reject(err);
+          else resolve(val);
+        });
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+const client = new RedisClient();
+module.exports = client;
