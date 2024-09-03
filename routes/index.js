5ce0aa261a7ddfdd39140a9dbd993ee51663227b
franklinAnozie <cfanozie@gmail.com>
2024-09-03 15:35:33 +0100

    not done

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import AppController from '../controllers/AppController';
-import UserController from '../controllers/UsersController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const routes = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
-  app.post('/users', UserController.postNew);
+  app.get('/connect', AuthController.getConnect);
+  app.get('/disconnect', AuthController.getDisconnect);
+  app.get('/users/me', UsersController.getMe);
+  app.post('/users', UsersController.postNew);
 };
 
 export default routes;
